b69b3333c40169e389135a15d49025b31a3a93f8
Asilvazavala <asilvazavala@gmail.com>
2023-11-09 13:23:02 -0600

    added settings

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -13,6 +13,11 @@ export function MainNav({
 
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: 'Panel',
+      active: pathname === `/${params.storeId}`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: 'Configuración',
       active: pathname === `/${params.storeId}/settings`,
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,32 @@
-import { UserButton } from "@clerk/nextjs";
-import { MainNav } from "@/components/MainNav";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const Navbar: React.FC = () => {
+import { MainNav } from "@/components/MainNav";
+import StoreSwitcher from "@/components/Store-switcher";
+import prismadb from '@/lib/prismadb';
+
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId: userId
+    },
+  });
+
   return (
     <section className="border-b">
       <nav className="flex h-16 items-center px-4">
-        <article>
-          Store switcher
-        </article>
-        
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
 
         <small className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
         </small>
-
       </nav>
     </section>
   )
