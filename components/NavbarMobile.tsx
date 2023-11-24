ec3313f4b69911610f207906f54a00135a9ad5ee
Asilvazavala <asilvazavala@gmail.com>
2023-11-24 17:23:43 -0600

    modified height menu responsive

--- components/NavbarMobile.tsx
+++ components/NavbarMobile.tsx
@@ -1,7 +1,6 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoClose } from "react-icons/io5";
 import { Routes } from "./MainNav";
-
 interface NavbarMobileProps {
   routes: Routes;
 }
@@ -16,12 +15,13 @@ export const NavbarMobile: React.FC<NavbarMobileProps> = ({
       <label htmlFor="menuMobile">
         <input type='checkbox' id='menuMobile' className='hidden peer' />
         <GiHamburgerMenu className='h-8 w-8 peer-checked:scale-0 absolute top-4 right-4 z-50
-        transition duration-700 delay-150 peer-checked:delay-0' />
+        transition duration-700 delay-100 peer-checked:delay-0' />
         <IoClose className='h-8 w-8 scale-0 dark:text-white text-black peer-checked:block z-50 
-        peer-checked:scale-100 absolute top-4 right-4 transition duration-700 delay-0 peer-checked:delay-150' />
+        peer-checked:scale-100 absolute top-4 right-4 transition duration-700 delay-0 peer-checked:delay-100' />
         
         <ul className='z-40 translate-x-[-150%] w-full absolute right-0 top-0 transition duration-700 
-        pt-20 flex flex-col gap-y-8 bg-gradient-to-r from-slate-400 to-slate-600 dark:from-slate-800 dark:to-slate-950 peer-checked:translate-x-0'>
+        pt-20 flex flex-col gap-y-8 bg-gradient-to-r from-slate-400 to-slate-600 dark:from-slate-800 dark:to-slate-950 
+        peer-checked:translate-x-0 overflow-auto h-screen'>
           {routes.map(item => (
             <li key={item.label} className="flex justify-between w-full text-3xl py-2
             border-gray-300 border-b px-8">
